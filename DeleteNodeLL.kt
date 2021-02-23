43a90fab2b4ee9c9e22cc0bc4eba900854f5f7f3
Samim Hakimi <30713537+sammyHa@users.noreply.github.com>
2021-02-23 15:06:59 -0800

    Add files via upload

---
+++ DeleteNodeLL.kt
@@ -0,0 +1,23 @@
+package leetCoding
+
+/**
+ * Delete the node in a linked list
+ *
+ * Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+    It is guaranteed that the node to be deleted is not a tail node in the list.
+ *
+ * Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+ */
+
+
+fun deleteNode(node:Node){
+    node?:return // check if node is null then just return
+
+   /**  4-->5-->1-->6-->9 here delete the 5 */
+    node.data = node.next!!.data /** override the value of current node with the next node value*/
+    node.next = node.next!!.next /** assign the pointer to the next.next node */
+
+
+}
